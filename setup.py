e86cba501a30ee547463904bc985f2c6415c6d93
deepjia <deepjia@gmail.com>
2018-06-08 17:28:45 +0800

    Bump version 0.1.3.1

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ OPTIONS = {
         'CFBundleName': APP_NAME,
         'CFBundleDisplayName': APP_NAME,
         'LSUIElement': True,
-        'CFBundleVersion': "0.1.3",
-        'CFBundleShortVersionString': "0.1.3"
+        'CFBundleVersion': "0.1.3.1",
+        'CFBundleShortVersionString': "0.1.3.1"
     }
 }
 
